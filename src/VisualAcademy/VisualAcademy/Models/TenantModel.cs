fd694f00c6cfddc8ff2c728410e10e53653c2b0f
Park YongJune <redplus@redplus.net>
2023-03-15 15:16:13 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Models/TenantModel.cs
+++ src/VisualAcademy/VisualAcademy/Models/TenantModel.cs
@@ -1,31 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace VisualAcademy.Models {
-    public class TenantModel {
-        public long Id { get; set; }
+namespace VisualAcademy.Models; 
+public class TenantModel {
+    public long Id { get; set; }
 
-        [Display(Name = "Name")]
-        public string? Name { get; set; } = null!;
+    [Display(Name = "Name")]
+    public string? Name { get; set; } = null!;
 
-        [Display(Name = "Connection String")]
-        public string? ConnectionString { get; set; }
+    [Display(Name = "Connection String")]
+    public string? ConnectionString { get; set; }
 
-        [Display(Name = "Authentication Header")]
-        public string? AuthenticationHeader { get; set; }
+    [Display(Name = "Authentication Header")]
+    public string? AuthenticationHeader { get; set; }
 
-        [Display(Name = "Account ID")]
-        public string? AccountID { get; set; }
+    [Display(Name = "Account ID")]
+    public string? AccountID { get; set; }
 
-        [Display(Name = "Global Search Connection String")]
-        public string? GSConnectionString { get; set; }
+    [Display(Name = "Global Search Connection String")]
+    public string? GSConnectionString { get; set; }
 
-        [Display(Name = "Report Writer URL")]
-        public string? ReportWriterURL { get; set; }
+    [Display(Name = "Report Writer URL")]
+    public string? ReportWriterURL { get; set; }
 
-        [Display(Name = "Badge Photo Type")]
-        public string? BadgePhotoType { get; set; }
+    [Display(Name = "Badge Photo Type")]
+    public string? BadgePhotoType { get; set; }
 
-        [Display(Name = "Portal Name")]
-        public string? PortalName { get; set; }
-    }
+    [Display(Name = "Portal Name")]
+    public string? PortalName { get; set; }
 }
