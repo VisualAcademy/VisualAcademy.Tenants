f35adfc23d1b21ad90bffad907bf9622d56f6a0b
Park YongJune <redplus@redplus.net>
2023-03-18 05:14:47 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
@@ -3,63 +3,62 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Tenants
+namespace VisualAcademy.Pages.Tenants;
+
+public class EditModel : PageModel
 {
-    public class EditModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public EditModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    [BindProperty]
+    public TenantModel TenantModel { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(long? id)
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
-
-        public EditModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
-
-        [BindProperty]
-        public TenantModel TenantModel { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(long? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
 
-            var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
-            if (tenantmodel == null)
-            {
-                return NotFound();
-            }
-            TenantModel = tenantmodel;
+        var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
+        if (tenantmodel == null)
+        {
+            return NotFound();
+        }
+        TenantModel = tenantmodel;
+        return Page();
+    }
+
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        _context.Attach(TenantModel).State = EntityState.Modified;
+
+        try
         {
-            if (!ModelState.IsValid)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TenantModelExists(TenantModel.Id))
             {
-                return Page();
+                return NotFound();
             }
-
-            _context.Attach(TenantModel).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+                throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TenantModelExists(TenantModel.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
         }
 
-        private bool TenantModelExists(long id) => _context.Tenants.Any(e => e.Id == id);
+        return RedirectToPage("./Index");
     }
+
+    private bool TenantModelExists(long id) => _context.Tenants.Any(e => e.Id == id);
 }
