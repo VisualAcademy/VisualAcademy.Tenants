af51c2631d13770e051de55a7a6e2ace8075a38c
Park YongJune <redplus@redplus.net>
2023-03-17 19:07:52 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Index.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Index.cshtml.cs
@@ -2,16 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Tenants
+namespace VisualAcademy.Pages.Tenants;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
-    {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
 
-        public IndexModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+    public IndexModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
 
-        public IList<TenantModel> TenantModel { get; set; } = default!;
+    public IList<TenantModel> TenantModel { get; set; } = default!;
 
-        public async Task OnGetAsync() => TenantModel = await _context.Tenants.ToListAsync();
-    }
+    public async Task OnGetAsync() => TenantModel = await _context.Tenants.ToListAsync();
 }
