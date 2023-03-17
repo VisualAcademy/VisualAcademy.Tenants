0d702b825c098a28fe3cc27b09431bc56ee6c659
Park YongJune <redplus@redplus.net>
2023-03-18 05:14:26 +0900

    Use expression body for constructors

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Edit.cshtml.cs
@@ -9,10 +9,7 @@ namespace VisualAcademy.Pages.Tenants
     {
         private readonly VisualAcademy.Data.ApplicationDbContext _context;
 
-        public EditModel(VisualAcademy.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        public EditModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
 
         [BindProperty]
         public TenantModel TenantModel { get; set; } = default!;
