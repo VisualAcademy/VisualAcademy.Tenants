83d466b1dc3344aff3603f9dc374ab327c279139
Park YongJune <redplus@redplus.net>
2023-03-18 05:15:07 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Details.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Details.cshtml.cs
@@ -3,33 +3,32 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Tenants
+namespace VisualAcademy.Pages.Tenants;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public DetailsModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    public TenantModel TenantModel { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(long? id)
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
-
-        public DetailsModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
-
-        public TenantModel TenantModel { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(long? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
-            if (tenantmodel == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                TenantModel = tenantmodel;
-            }
-            return Page();
+            return NotFound();
         }
+
+        var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
+        if (tenantmodel == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            TenantModel = tenantmodel;
+        }
+        return Page();
     }
 }
