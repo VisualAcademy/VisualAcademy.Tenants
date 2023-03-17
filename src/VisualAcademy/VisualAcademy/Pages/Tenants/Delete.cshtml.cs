544a751fbf9fcbf881c756bcfd585cc850734852
Park YongJune <redplus@redplus.net>
2023-03-18 05:15:35 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Delete.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Delete.cshtml.cs
@@ -3,53 +3,52 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Tenants
+namespace VisualAcademy.Pages.Tenants;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public DeleteModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    [BindProperty]
+    public TenantModel TenantModel { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(long? id)
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
-
-        public DeleteModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
-
-        [BindProperty]
-        public TenantModel TenantModel { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(long? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (tenantmodel == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                TenantModel = tenantmodel;
-            }
-            return Page();
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(long? id)
+        var tenantmodel = await _context.Tenants.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (tenantmodel == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tenantmodel = await _context.Tenants.FindAsync(id);
-            if (tenantmodel != null)
-            {
-                TenantModel = tenantmodel;
-                _context.Tenants.Remove(TenantModel);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return NotFound();
         }
+        else
+        {
+            TenantModel = tenantmodel;
+        }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(long? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var tenantmodel = await _context.Tenants.FindAsync(id);
+        if (tenantmodel != null)
+        {
+            TenantModel = tenantmodel;
+            _context.Tenants.Remove(TenantModel);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
