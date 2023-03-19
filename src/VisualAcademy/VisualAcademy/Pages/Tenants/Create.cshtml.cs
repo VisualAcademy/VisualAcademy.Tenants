b080b0b7742a674b52788fb8acd843a58b7d048b
Park YongJune <redplus@redplus.net>
2023-03-19 11:49:33 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy/Pages/Tenants/Create.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/Tenants/Create.cshtml.cs
@@ -2,31 +2,30 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Tenants
+namespace VisualAcademy.Pages.Tenants;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public CreateModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    public IActionResult OnGet() => Page();
+
+    [BindProperty]
+    public TenantModel TenantModel { get; set; } = default!;
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
-
-        public CreateModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
-
-        public IActionResult OnGet() => Page();
-
-        [BindProperty]
-        public TenantModel TenantModel { get; set; } = default!;
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        if (!ModelState.IsValid)
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Tenants.Add(TenantModel);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        _context.Tenants.Add(TenantModel);
+        await _context.SaveChangesAsync();
+
+        return RedirectToPage("./Index");
     }
 }
