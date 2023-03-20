8c1c0e569e078e7d82506d382a257cad8af962cb
Park YongJune <redplus@redplus.net>
2023-03-20 17:16:48 +0900

    파일 범위 네임스페이스로 변환

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Create.cshtml.cs
@@ -3,35 +3,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.AppointmentsTypes
+namespace VisualAcademy.Pages.AppointmentsTypes;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public CreateModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    public IActionResult OnGet()
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
+        ViewData["TenantId"] = new SelectList(_context.Tenants, "Id", "Name");
+        return Page();
+    }
 
-        public CreateModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+    [BindProperty]
+    public AppointmentTypeModel AppointmentTypeModel { get; set; } = default!;
 
-        public IActionResult OnGet()
-        {
-            ViewData["TenantId"] = new SelectList(_context.Tenants, "Id", "Name");
-            return Page();
-        }
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
+    {
+        //if (!ModelState.IsValid)
+        //{
+        //    return Page();
+        //}
 
-        [BindProperty]
-        public AppointmentTypeModel AppointmentTypeModel { get; set; } = default!;
+        _context.AppointmentsTypes.Add(AppointmentTypeModel);
+        await _context.SaveChangesAsync();
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
-        {
-            //if (!ModelState.IsValid)
-            //{
-            //    return Page();
-            //}
-
-            _context.AppointmentsTypes.Add(AppointmentTypeModel);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
 }
