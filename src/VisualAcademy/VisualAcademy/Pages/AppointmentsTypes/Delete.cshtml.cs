e5a4d62c98f422761e5a02e24a3dcab216e40bf2
Park YongJune <redplus@redplus.net>
2023-03-20 17:17:29 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Delete.cshtml.cs
+++ src/VisualAcademy/VisualAcademy/Pages/AppointmentsTypes/Delete.cshtml.cs
@@ -3,53 +3,52 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.AppointmentsTypes
+namespace VisualAcademy.Pages.AppointmentsTypes;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly VisualAcademy.Data.ApplicationDbContext _context;
+
+    public DeleteModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
+
+    [BindProperty]
+    public AppointmentTypeModel AppointmentTypeModel { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(long? id)
     {
-        private readonly VisualAcademy.Data.ApplicationDbContext _context;
-
-        public DeleteModel(VisualAcademy.Data.ApplicationDbContext context) => _context = context;
-
-        [BindProperty]
-        public AppointmentTypeModel AppointmentTypeModel { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(long? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var appointmenttypemodel = await _context.AppointmentsTypes.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (appointmenttypemodel == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                AppointmentTypeModel = appointmenttypemodel;
-            }
-            return Page();
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(long? id)
+        var appointmenttypemodel = await _context.AppointmentsTypes.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (appointmenttypemodel == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var appointmenttypemodel = await _context.AppointmentsTypes.FindAsync(id);
-            if (appointmenttypemodel != null)
-            {
-                AppointmentTypeModel = appointmenttypemodel;
-                _context.AppointmentsTypes.Remove(AppointmentTypeModel);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return NotFound();
         }
+        else
+        {
+            AppointmentTypeModel = appointmenttypemodel;
+        }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(long? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var appointmenttypemodel = await _context.AppointmentsTypes.FindAsync(id);
+        if (appointmenttypemodel != null)
+        {
+            AppointmentTypeModel = appointmenttypemodel;
+            _context.AppointmentsTypes.Remove(AppointmentTypeModel);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
